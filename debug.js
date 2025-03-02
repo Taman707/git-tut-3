b9a9a9fde8bdc19fa74b84614b02dd2e7fa4d9a5
Tamanpreet Singh <tamanpreetsingh76@gmail.com>
2025-03-02 21:42:17 +0530

    Debugged-commit-Web-Application

---
+++ debug.js
@@ -0,0 +1,2 @@
+console.log('This is the debugged code ');
+// Now , we need to add this into our website so the current bug will be easily fixed and it must not intervene with the development of feature
